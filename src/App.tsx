21dbb3fa018ad862608a3af4fdd639dd636c6899
Matheusfreitas <rabelo210@gmail.com>
2024-06-23 22:35:56 -0300

    created solution page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,38 @@
-import { Form } from "./sections/Form";
-import { Home } from "./sections/Home";
+import { useState } from 'react';
+// import { Button } from './components/Button'
+import Solucoes from './components/solucoes';
 
 export default function App() {
+  // const [count, setCount] = useState(0)
 
+
+  // const addCount = () => {
+  //   setCount(count + 1)
+  // }
+
+  // const subCount = () => {
+  //   setCount(count - 1)
+  // }
+
+  // const resetCount = () => {
+  //   setCount(0)
+  // }
   return (
-    <div className="flex w-full h-auto flex-col items-center font-sans">
-      <Home />
-      <Form />
+    <div className="">
+
+      {/* <div className='flex flex-col gap-4 w-full max-w-96'>
+        <div className='flex justify-center items-center'>
+          <h2 className='text-white font-semibold text-2xl'>{count}</h2>
+        </div>
+        <Button name='Aumentar' handleButton={addCount} />
+
+        <Button name='Diminuir' handleButton={subCount} />
+
+        <Button name='Rezetar' handleButton={resetCount} />
+      </div> */}
+
+
+      <Solucoes />
     </div>
   )
 }
-
--- src/utils/ui/Route.ts
+++
@@ -1,7 +0,0 @@
-export const RoutePaths = {
-  home: "#home",
-  about: "#about",
-  content: "#content",
-  doubts: "#doubts",
-  play: "#play",
-} as const;
